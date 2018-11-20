05b8734315d3395283b994bd2ca7d2c3270b38fa
Andrew MacLeod <andrewmorrismacleod@googlemail.com>
2018-11-20 17:05:56 +0000

    Test 9&10 Complete :)

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,4 +1,4 @@
-# require('Date')
+require('date')
 
 def return_10
   return 10
@@ -33,31 +33,5 @@ def add_string_as_number(string_1, string_2)
 end
 
 def number_to_full_month_name(number)
-# Date::MONTHNAMES(number)
-  case number
-  when 1
-    return "January"
-  when 2
-    return "February"
-  when 3
-    return "March"
-  when 4
-    return "April"
-  when 5
-    return "May"
-  when 6
-    return "June"
-  when 7
-    return "July"
-  when 8
-    return "August"
-  when 9
-    return "September"
-  when 10
-    return "October"
-  when 11
-    return "November"
-  when 12
-    return "December"
-  end
+Date::MONTHNAMES[number]
 end
