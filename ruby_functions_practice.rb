6051fc276a38315a141e3a58cb3ac265ed722840
Andrew MacLeod <andrewmorrismacleod@googlemail.com>
2018-11-20 17:04:09 +0000

    Test 8 Complete

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,3 +1,5 @@
+# require('Date')
+
 def return_10
   return 10
 end
@@ -26,6 +28,36 @@ def join_string(string_1, string_2)
   return string_1 + string_2
 end
 
-def add_string_as_number(string_1, string_2)  
+def add_string_as_number(string_1, string_2)
   return add(string_1.to_i, string_2.to_i)
+end
+
+def number_to_full_month_name(number)
+# Date::MONTHNAMES(number)
+  case number
+  when 1
+    return "January"
+  when 2
+    return "February"
+  when 3
+    return "March"
+  when 4
+    return "April"
+  when 5
+    return "May"
+  when 6
+    return "June"
+  when 7
+    return "July"
+  when 8
+    return "August"
+  when 9
+    return "September"
+  when 10
+    return "October"
+  when 11
+    return "November"
+  when 12
+    return "December"
+  end
 end
